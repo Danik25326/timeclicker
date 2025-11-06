48b04e1cf811fe9ae12728eff1d5a69dcdd15cb1
Данило <danikrut79@gmail.com>
2025-11-06 20:26:52 +0200

    Update musicList.js

--- musicList.js
+++ musicList.js
@@ -1,10 +1,1 @@
-const tracks = [
-  {
-    name: "📼 KSLV Noh - Tokyo Drift Remix",
-    src: "https://cdn.pixabay.com/download/audio/2022/10/13/audio_38222eec5f.mp3"
-  },
-  {
-    name: "🏎️ LXST CXNTURY - Crash My Mind",
-    src: "https://cdn.pixabay.com/download/audio/2023/03/24/audio_91899cf45e.mp3"
-  }
-];
+
